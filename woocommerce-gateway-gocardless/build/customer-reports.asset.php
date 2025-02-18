c9d77a34ae3059a68f627a1f66eb4bb3485004aa
Sean Murphy <sean@iamseanmurphy.com>
2025-02-18 16:19:49 +0000

    2.9.2

--- woocommerce-gateway-gocardless/build/customer-reports.asset.php
+++ woocommerce-gateway-gocardless/build/customer-reports.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-hooks', 'wp-i18n'), 'version' => '046282a2bb220dadf66b');
+<?php return array('dependencies' => array('react', 'wp-hooks', 'wp-i18n'), 'version' => 'db5d48cee89928d91b39');
--- woocommerce-gateway-gocardless/build/index.asset.php
+++ woocommerce-gateway-gocardless/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '20f908f70374a2455736');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '494e4afed799f2cde0fa');
