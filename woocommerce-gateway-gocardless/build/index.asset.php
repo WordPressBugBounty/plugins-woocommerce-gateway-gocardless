94b73d437485ecaf601c248b1dc7c4d624aae9e2
Sean Murphy <sean@iamseanmurphy.com>
2025-07-02 16:18:18 +0000

    2.9.6

--- woocommerce-gateway-gocardless/build/index.asset.php
+++ woocommerce-gateway-gocardless/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '494e4afed799f2cde0fa');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => '70c5c2023330429d40bb');
